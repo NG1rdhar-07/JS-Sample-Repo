5dfa8e1fbd7671245f5a5c411e1d3bc1ee5fb84f
Noor <noorgirdhar705@gmail.com>
2025-10-30 02:53:55 +0530

    random

--- dataTypes.js
+++ dataTypes.js
@@ -3,3 +3,14 @@ let abc = 13
 let prw = "No"
 
 console.log([typeof "Noor", abc, prw])
+
+const heros = ["bheem", "doga", "Noor"];
+
+let myObj = {
+    name: "Random",
+    age : 843
+}
+
+const xyz = function(){
+    console.log(0);
+}
