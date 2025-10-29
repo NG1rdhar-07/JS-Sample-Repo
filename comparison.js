8a737e4d06200f759559fa370b5f14d67effd8c0
Noor <noorgirdhar705@gmail.com>
2025-10-30 02:26:45 +0530

    more

---
+++ comparison.js
@@ -0,0 +1,5 @@
+console.log(2>3);
+console.log(1 == "1");
+
+console.log(null == 0);
+console.log(null > 0);
---
+++ conversion.js
@@ -0,0 +1,25 @@
+// let score = "3434";
+
+// console.log(typeof score);
+
+// let valInNum = Number(score);
+// console.log(typeof valInNum);
+
+// let someNum = 543
+// let stringNumber = String(someNum)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// console.log(2^9);
+
+let str1 = "Noor"
+let str2 = "Girdhar"
+
+let str3 = str1 + " " +str2
+console.log(str3)
+
+console.log(1 + 2 + "3")
+console.log("1" + 2 + 3)
+
+console.log(+true)
+console.log(+"")
